3fce43a23b06ee30183587302f48dad252b992ab
Paulo Abarca <70716311+pabarcam@users.noreply.github.com>
2022-07-03 23:04:17 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<div class="m-0 vh-100 row justify-content-center align-items-center col-auto  p-5 text-center">
+    <input type="text" class="form-control" placeholder="Usuario" aria-label="Username" aria-describedby="basic-addon1">
+</div>
+`;
